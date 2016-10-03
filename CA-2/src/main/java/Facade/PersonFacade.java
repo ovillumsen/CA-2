29fcfb3ad725a9ff39f9e51a0e4e37dc3d93b020
Thesoap <tvh_1994@hotmail.dk>
2016-10-03 10:45:05 +0200

    Entity og PersonFacade start

--- CA-2/src/main/java/Facade/PersonFacade.java
+++ CA-2/src/main/java/Facade/PersonFacade.java
@@ -5,10 +5,39 @@
  */
 package Facade;
 
+import Entity.Person;
+import Interface.IPersonFacade;
+import java.util.List;
+
 /**
  *
  * @author Thesoap
  */
-public class PersonFacade {
+public class PersonFacade implements IPersonFacade {
+
+    public boolean addPerson(Person person) {
+        return true;
+    }
+
+    public Person getPerson(int ID) {
+        Person p = new Person();
+        return p;
+    }
+
+    public List<Person> getPersons() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<Person> getPersons(int zipcode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Boolean deletePerson(int ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Boolean editPerson(Person person, int ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }

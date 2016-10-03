e2b288b9bccc2a6eca803b925cc2159b3b6b1715
Oliver <Oliver@10.50.132.5>
2016-10-03 11:20:45 +0200

    Lægger projektet op

---
+++ CA-2/src/main/java/Entity/asd.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author Oliver
+ */
+public class asd {
+    public String string = "fuck det her";
+}

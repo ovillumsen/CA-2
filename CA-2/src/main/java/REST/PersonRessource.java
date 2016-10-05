6115b0e121052fb28bb8578ad9aff0b161636636
michael2750 <2750daugbjerg@gmail.com>
2016-10-05 10:26:31 +0200

    api get works

--- CA-2/src/main/java/REST/PersonRessource.java
+++ CA-2/src/main/java/REST/PersonRessource.java
@@ -5,6 +5,10 @@
  */
 package REST;
 
+import Entity.Person;
+import Facade.PersonFacade;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -12,6 +16,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -33,17 +38,27 @@ public class PersonRessource {
 
     /**
      * Retrieves representation of an instance of REST.PersonRessource
+     *
      * @return an instance of java.lang.String
      */
     @GET
+    @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        //TODO return proper representation object
-        return "Hellooooo";
+    public String getJson(@PathParam("id") int id) {
+        Gson gson = new com.google.gson.GsonBuilder().create();
+        JsonObject person = new JsonObject();
+        PersonFacade PF = new PersonFacade();
+        Person p = PF.getPerson(id);
+        person.addProperty("Firstname", p.getFn());
+        person.addProperty("Firstname", p.getLn());
+        String jsonRes = gson.toJson(person);
+        String lort = p.getFn()+ " " +p.getLn();
+        return jsonRes;
     }
 
     /**
      * PUT method for updating or creating an instance of PersonRessource
+     *
      * @param content representation for the resource
      */
     @PUT
